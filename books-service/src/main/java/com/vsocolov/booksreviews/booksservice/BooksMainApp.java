a7a9a055d759adc2382a8288a95955320ad4af51
Vladislav Socolov <vladislav.socolov@ynap.com>
2018-05-14 16:10:22 +0100

    Added two subprojects

---
+++ books-service/src/main/java/com/vsocolov/booksreviews/booksservice/BooksMainApp.java
@@ -0,0 +1,7 @@
+package com.vsocolov.booksreviews.books;
+
+public class BooksMainApp {
+  public static void main(final String[] args) {
+    System.out.println("Hello from books service");
+  }
+}
---
+++ users-service/src/main/java/com/vsocolov/booksreviews/usersservice/UsersMainApp.java
@@ -0,0 +1,7 @@
+package com.vsocolov.booksreviews.usersservice;
+
+public class UsersMainApp {
+  public static void main(final String[] args) {
+    System.out.println("Hello from users-service");
+  }
+}
