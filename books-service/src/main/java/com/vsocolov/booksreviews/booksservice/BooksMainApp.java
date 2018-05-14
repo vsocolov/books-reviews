03e3105a85ca90373688c3607d50dc393b96c7e7
Vladislav Socolov <vladislav.socolov@ynap.com>
2018-05-14 16:15:00 +0100

    Added a simple test

--- books-service/src/main/java/com/vsocolov/booksreviews/booksservice/BooksMainApp.java
+++ books-service/src/main/java/com/vsocolov/booksreviews/booksservice/BooksMainApp.java
@@ -1,7 +1,7 @@
-package com.vsocolov.booksreviews.books;
+package com.vsocolov.booksreviews.booksservice;
 
 public class BooksMainApp {
   public static void main(final String[] args) {
-    System.out.println("Hello from books service");
+    System.out.println("Hello from books-service");
   }
 }
--- books-service/src/test/java/com/vsocolov/booksreviews/booksservice/BooksMainAppTest.java
+++ books-service/src/test/java/com/vsocolov/booksreviews/booksservice/BooksMainAppTest.java
@@ -1,5 +1,15 @@
-import static org.junit.Assert.*;
+package com.vsocolov.booksreviews.booksservice;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class BooksMainAppTest {
 
+  @Test
+  public void test() {
+    //nothing
+  }
 }
